f7ea6f48016c7bc44400bcf5ba53f38ad8e6318d
Olivier Cuny <ogmios2@gmail.com>
2024-12-24 15:01:16 -0800

    Update pip and related scripts; remove obsolete files and adjust shebangs

--- .venv/bin/activate.fish
+++ .venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/olivier/gemini-video-analyzer/.venv"
+set -gx VIRTUAL_ENV "/Users/olivier/Dropbox/Main Files/Software Dev/Projects - Misc/gemini-video-analyzer/.venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
